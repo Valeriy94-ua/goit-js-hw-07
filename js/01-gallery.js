2a0426684a116e219e06ee2428c704d0d4e4fa33
Valeriy94-ua <128257210+Valeriy94-ua@users.noreply.github.com>
2023-05-24 15:14:02 +0300

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// find gallery?
+const galleryList = document.querySelector('.gallery');
+// create card
+const makeGalleryList = ({ original, preview, description }) => {
+  return `<li class="gallery__item">
+            <a class="gallery__link" data-source="${original}" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </li>`;
+};
+// iteration massive
+const makeGalleryArr = galleryItems.map(imageInfo => {
+  return makeGalleryList(imageInfo);
+});
+// add in html
+galleryList.innerHTML = makeGalleryArr.join('');
+// add action
+
+const galleryLinks = document.querySelectorAll('.gallery__link');
+
+galleryLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+
+    const instance = basicLightbox.create(`
+      <img
+      src="${event.target.dataset.source}" 
+      alt="${event.target.alt}" 
+      width = "800" height = "600" >
+    `);
+    instance.show();
+  });
+});
+//
+//
+// import * as basicLightbox from 'basiclightbox';
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `);
+
+// instance.show();
+//
